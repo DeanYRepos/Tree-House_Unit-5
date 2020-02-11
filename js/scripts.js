76b755306fa08f23da8a0b68b02b99085392e8e9
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-02-10 23:14:15 -0600

    Added generateGallery() function

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,48 @@
 
 const div = document.createElement('div');
 const formDiv = document.getElementsByClassName('search-container');
-div.className = 'card';
-function fetch(URL){
-fetch( "https://randomuser.me/api/?nat=US&results=12")
-.then(response => response.json())
-.then(data => generateForm(data));
-}
+const gallery = document.getElementById('gallery');
+function fetchData(URL){
+    return fetch(URL)
+    
+.then(response => response.json());
 
-function generateForm(data){
+}
+fetchData("https://randomuser.me/api/?nat=US&results=12")
+.then(data => {
+generateGallery(data);
+
+});
+function generateForm(){
 
 const form = ` <form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
 <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form>`;
-formDiv.innerHTML = form;
+formDiv.innerHTML += form;
+
+
+}
+
+function generateGallery(data){
+     let users = data.results;
+   users.map(person => {
+
+   const galleryDiv =      
+  `
+<div class="card">
+    <div class="card-img-container">
+        <img class="card-img" src=${person.picture.large} alt=${person}
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+        <p class="card-text">${person.email}</p>
+        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+    </div>
+</div>`;
+gallery.innerHTML += galleryDiv;
+    });
+
 
 
 }
