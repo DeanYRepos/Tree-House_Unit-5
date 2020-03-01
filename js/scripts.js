cb1e05687ae653a76696220de74cfe61d5294f2f
DeansRepos <54600923+DeanYRepos@users.noreply.github.com>
2020-03-01 13:02:07 -0600

    Finished product for meets expectation grade....
    added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
 const formDiv = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 const body = document.querySelector('body');
-//let user = [];
-const xButton = document.getElementById('modal-close-btn');
+let cards = document.querySelectorAll('.card');
 const containerDiv = document.createElement('DIV');
 
 function fetchData(URL) { //reusable fetch function, parses user to JSON
@@ -11,16 +10,16 @@ function fetchData(URL) { //reusable fetch function, parses user to JSON
         .then(response => response.json())
         .catch(error => console.log('404 there was a problem!', error));
 }
-//fetchData("https://randomuser.me/api/?nat=US&results=12")
-fetchData("https://fsjs-public-api-backup.herokuapp.com/api")
+fetchData("https://randomuser.me/api/?nat=US&results=12")       //fetch data function, fetches 12 users iteratates users in callback function
+                                                                // calls functions to generate gallery, form and, event handler
     .then(user => {
-        generateGallery(user);
-        eventListener(user);
+        generateGallery(user.results);
+        eventListener(user.results);
 
         generateForm();
     });
 
-function checkStatus(response) {
+function checkStatus(response) {     //function checks status of promise and returns response
     if (response.ok) {
         return Promise.resolve(response);
 
@@ -30,27 +29,21 @@ function checkStatus(response) {
 
 }
 
-function generateForm() {
-
+function generateForm() {   // function generates form for search bar
 
     const form = ` <form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
 <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form>`;
 
-
     formDiv.innerHTML += form;
 
 }
 
-function generateGallery(user) { //Generates and displays user to gallery div
-    user = user.results;
+function generateGallery(user) { //Generates and displays users to gallery div
 
-
-    user.map(person => {
-
-        const galleryDiv =
-            `
+    const galleryDiv = user.map(person => {
+        return `
 <div class="card" >
     <div class="card-img-container">
         <img class="card-img" src=${person.picture.large} alt="profile picture">
@@ -61,146 +54,63 @@ function generateGallery(user) { //Generates and displays user to gallery div
         <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
     </div>
 </div>`;
-        gallery.innerHTML += galleryDiv;
 
     });
+    gallery.innerHTML += galleryDiv;
 
 }
 
-
-function generateModal(user) {
-
-    //users = user.results;
-    console.log(user);
-
-
-    // user.map(person => {
+function generateModal(user, i) { //generates and displays user modal to created div element
 
     let html = `
  <div class="modal-container">
 <div class="modal">
     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
     <div class="modal-info-container">
-        <img class="modal-img" src=${user.picture.large} alt="profile picture">
-        <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-        <p class="modal-text">${user.email}</p>
-        <p class="modal-text cap">${user.location.city}</p>
+        <img class="modal-img" src=${user[i].picture.large} alt="profile picture">
+        <h3 id="name" class="modal-name cap">${user[i].name.first} ${user[i].name.last}</h3>
+        <p class="modal-text">${user[i].email}</p>
+        <p class="modal-text cap">${user[i].location.city}</p>
         <hr>
-        <p class="modal-text">${user.phone}</p>
-        <p class="modal-text">${user.location.street.number}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
-        <p class="modal-text">Birthday ${user.dob.date}</p>
+        <p class="modal-text">${user[i].phone}</p>
+        <p class="modal-text">${user[i].location.street.number}, ${user[i].location.city}, ${user[i].location.state} ${user[i].location.postcode}</p>
+        <p class="modal-text">Birthday ${user[i].dob.date.slice(0, 10)}</p>
     </div>
 </div>
 
 `;
-    userInfo = html;
+    containerDiv.innerHTML = html;
+    return containerDiv;
+}
+// // IMPORTANT: Below is only for exceeds tasks 
+// <div class="modal-btn-container">
+//     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+//     <button type="button" id="modal-next" class="modal-next btn">Next</button>
+// </div>
+// </div>
 
-    // // IMPORTANT: Below is only for exceeds tasks 
-    // <div class="modal-btn-container">
-    //     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-    //     <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    // </div>
-    // </div>
+
+function eventListener(user) {  //function iterates through users cards, when clicked modal function is called and appended to body
+                                //close modal function called to close modal when X button is clicked
+    let cards = document.querySelectorAll('.card');
+
+    console.log('handler');
 
 
-    // }).join('');
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', () => {
+            console.log(user);
+            body.appendChild(generateModal(user, i));
+            closeModal();
 
-    containerDiv.innerHTML = userInfo;
+        })
+    }
+}
 
-    gallery.appendChild(containerDiv);
-
+function closeModal() {     //function when called will close modal when X button is clicked
     const closeBtn = document.getElementById("modal-close-btn");
     closeBtn.addEventListener('click', () => {
         containerDiv.remove();
 
-    });
-
-}
-
-
-
-
-function eventListener(user) {
-    // let cards = document.querySelector('.card');
-    let users = user.results;
-
-    gallery.addEventListener('click', (e) => {
-        //console.log(e.target);
-        if (e.target.className === 'card') {
-            for (let key in users) {
-                //console.log(key);
-
-                if (key[name] === e.target[name]) {
-                   // generateModal(key);
-                    console.log(generateModal(key[name]));
-                    // }
-                }
-              //  console.log('if');
-
-            }
-
-        }
-
-
     })
 }
-
-//    if(e.target.index === users.index){
-
-//         console.log('if');
-//    }
-// if(e.target.className.includes('.card-info-container')){
-//        console.log(users);
-
-
-
-//     // console.log(user);
-//    for(let i = 0; i < users.length;i++){
-//        //console.log(users[i]);
-//        users.index = i;
-//        console.log(users.index);
-//       // let cards = document.querySelector('.card');
-//        gallery.addEventListener('click', (e) => {
-//            console.log(e.target);
-// if(users.index === e.target[i]){
-//     console.log('if');
-//        generateModal(users,i);
-// }
-//        })
-
-
-
-//let parseInt = parseInt(targetIndex,10);
-
-
-// console.log( generateModal(targetIndex));
-// generateModal(users);
-
-// users.forEach(card => {
-//     if (e.target.className.includes('card') && e.target === card) {
-//        // user.forEach(card => {
-//   console.log(users);
-
-//       //  })
-//         //generateModal(user);
-//   //   console.log(generateModal(e.target));
-//     }
-//     // console.log(e.target);
-// })
-
-
-// }
-
-// function eventListener(user) { //think about putting handler inside generate modal function to iterate 
-
-//     gallery.addEventListener('click', e => {
-
-//         if (e.target.className.includes('card')) {
-
-//           generateModal(user);
-
-
-//     }   
-
-// })
-// }
